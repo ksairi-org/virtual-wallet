7f4a1f438691ef24e061216317a2a732aac56bca
Mariano Ksairi <marianoksairi@gmail.com>
2025-09-25 16:54:06 -0300

    fix: fix warning on lingui

--- src/i18n/LinguiClientProvider/LinguiClientProvider.tsx
+++ src/i18n/LinguiClientProvider/LinguiClientProvider.tsx
@@ -1,12 +1,8 @@
 import type { I18nProviderProps } from "@lingui/react";
-
-import { useEffect } from "react";
-
+import { useEffect, useState } from "react";
 import { Platform, Settings } from "react-native";
-
 import { i18n } from "@lingui/core";
 import { I18nProvider } from "@lingui/react";
-
 import { setI18nLocale } from "../utils";
 import { getLocales } from "expo-localization";
 
@@ -15,14 +11,24 @@ type LinguiClientProviderProps = {
 };
 
 const LinguiClientProvider = ({ children }: LinguiClientProviderProps) => {
+  const [isI18nReady, setIsI18nReady] = useState(false);
+
   useEffect(() => {
-    const newLocale = getLocales()[0].languageCode;
-    if (Platform.OS === "ios") {
-      Settings.set({ AppleLanguages: newLocale });
-    }
-    console.log(newLocale);
-    setI18nLocale(newLocale);
+    const initializeI18n = () => {
+      const newLocale = getLocales()[0].languageCode;
+      if (Platform.OS === "ios") {
+        Settings.set({ AppleLanguages: newLocale });
+      }
+      setI18nLocale(newLocale);
+      setIsI18nReady(true);
+    };
+
+    initializeI18n();
   }, []);
+
+  if (!isI18nReady) {
+    return null;
+  }
 
   return <I18nProvider i18n={i18n}>{children}</I18nProvider>;
 };
